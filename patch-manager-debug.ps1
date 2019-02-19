ea9f53710c17f5031aee1fea8ee5300134e30f25
Jason Domican <jadomican@gmail.com>
2019-02-19 21:57:34 +0000

    Improved user output from script. Tested on PS 3, 4 and 5

--- patch-manager-debug.ps1
+++ patch-manager-debug.ps1
@@ -17,14 +17,14 @@ Write-Host "Attempting to download a patch baseline snapshot from S3..."
 $snap = Get-SSMDeployablePatchSnapshotForInstance -SnapshotId af258f6e-7371-472f-a69a-d3e56f2df9ae -InstanceId $instanceId -Verbose 
 (New-Object Net.WebClient).DownloadFile($snap.SnapshotDownloadUrl, "C:\pbsnapshot.json")
 
-if ($?) {write-Host ('File downloaded successfully from ' + $snap.SnapshotDownloadUrl)}
+if ($?) {write-Host ("`nFile downloaded successfully from " + $snap.SnapshotDownloadUrl)}
 
 #If the Amazon.PatchBaselineOperations.dll file does not exist in "C:\Program Files\Amazon\PatchBaselineOperations", it can be downloaded from here
 #https://s3-<region>.amazonaws.com/aws-ssm-<region>/patchbaselineoperations/Amazon.PatchBaselineOperations-1.12.zip
 
 if (-not (Test-Path $patchBaselineModuleLocation))
 {
-    Write-Host "PatchBaselineOperations not found, downloading now..."
+    Write-Host "`nPatchBaselineOperations not found, downloading now..."
     if(-not ($region.Equals('us-east-1')))
     {
         (New-Object Net.WebClient).DownloadFile("https://s3-$region.amazonaws.com/aws-ssm-$region/patchbaselineoperations/Amazon.PatchBaselineOperations-1.12.zip", "$patchBaselineModuleLocation.zip")
@@ -42,10 +42,11 @@ if (-not (Test-Path $patchBaselineModuleLocation))
         [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
     }
     Unzip "$patchBaselineModuleLocation.zip" "$patchBaselineModuleLocation"
+    Write-Host "`nPatchBaselineOperations downloaded, performing patch operation..."
 }
 else
 {
-    Write-Host 'PatchBaselineOperations found, performing patch operation...'
+    Write-Host "`nPatchBaselineOperations found, performing patch operation..."
 }
 
 #This calls the Patch Baseline operation manually with the -Debug option
